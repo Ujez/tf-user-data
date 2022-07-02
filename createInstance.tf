bcdc04a0eb4c1c9b9b3dd5bb96fe3c03aa04ffcb
ujez <ujezujez@gmail.com>
2022-07-02 19:29:28 +0100

    little update

--- createInstance.tf
+++ createInstance.tf
@@ -7,7 +7,6 @@ resource "aws_key_pair" "levelup_key3" {
 resource "aws_instance" "DockerInstance" {
   ami               = lookup(var.AMIS, var.AWS_REGION)
   instance_type     = "t2.micro"
-  availability_zone = "us-east-1a"
   key_name          = aws_key_pair.levelup_key3.key_name
 
   user_data = file("installapache.sh")
